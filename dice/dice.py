26d2020133947b3b44b1c59fa5890e16a148cb83
Clay Shoaf <clayshoaf@gmail.com>
2023-10-14 19:10:43 -0400

    cleaner animation code

--- dice/dice.py
+++ dice/dice.py
@@ -7,28 +7,26 @@ d6 = [dice.D6_FACE_1, dice.D6_FACE_2, dice.D6_FACE_3, dice.D6_FACE_4, dice.D6_FA
 def main():
     layout = [
               [sg.Image(d6[-1], key='-DIE-1-'), sg.Image(d6[-1], key='-DIE-2-')],
-              #[sg.Image(dice.D6_ANIMATION, key='-DIE-1-'), sg.Image(dice.D6_ANIMATION, key='-DIE-2-')],
               [sg.Button('Roll \'em', key='-ROLL-BUTTON-', border_width=0, pad=10, metadata=False)]
              ]
 
     window = sg.Window('Dice Roller', layout, element_justification='center')
 
-    roll = 0
     while True:  # Event Loop
-        event, values = window.read(timeout=10)
-        if roll > 0:
-            window['-DIE-1-'].update_animation(dice.D6_ANIMATION,  time_between_frames=10)
-            window['-DIE-2-'].update_animation(dice.D6_ANIMATION,  time_between_frames=10)
-            if roll == 1:
-                window['-DIE-1-'].update(source=d6[random.randint(0,len(d6)-1)])
-                window['-DIE-2-'].update(source=d6[random.randint(0,len(d6)-1)])
-            roll -= 1
+        event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
-        elif event == '-ROLL-BUTTON-':  # if the roll button is clicked
+        elif event == '-ROLL-BUTTON-':  
             window['-DIE-1-'].update(source=dice.D6_ANIMATION)
             window['-DIE-2-'].update(source=dice.D6_ANIMATION)
-            roll = 100
+            for _ in range(100):
+                window['-DIE-1-'].update_animation(dice.D6_ANIMATION,  time_between_frames=10)
+                window['-DIE-2-'].update_animation(dice.D6_ANIMATION,  time_between_frames=10)
+                window.read(timeout=10)
+            window['-DIE-1-'].update(source=d6[random.randint(0,len(d6)-1)])
+            window['-DIE-2-'].update(source=d6[random.randint(0,len(d6)-1)])
+
+
             
 
 
