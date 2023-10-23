ba80b4d44bbea332a5f050c8066c216a16f8df05
Clay Shoaf <clayshoaf@gmail.com>
2023-10-23 19:06:29 -0400

    trying to get pygbag to work

--- dice/dice.py
+++ dice/dice.py
@@ -7,7 +7,8 @@ d6 = [dice.D6_FACE_1, dice.D6_FACE_2, dice.D6_FACE_3, dice.D6_FACE_4, dice.D6_FA
 def main():
     layout = [
               [sg.Image(d6[-1], key='-DIE-1-'), sg.Image(d6[-1], key='-DIE-2-')],
-              [sg.Button('Roll \'em', key='-ROLL-BUTTON-', border_width=0, pad=10, metadata=False)]
+              [sg.Button('Roll \'em', key='-ROLL-BUTTON-', border_width=0, pad=10, metadata=False)],
+              [sg.Text('test')]
              ]
 
     window = sg.Window('Dice Roller', layout, element_justification='center')
@@ -17,6 +18,9 @@ def main():
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
         elif event == '-ROLL-BUTTON-':  
+            new_element = [sg.Text('New Text Element')]
+            layout.append(new_element)
+            window.Layout(layout)
             window['-DIE-1-'].update(source=dice.D6_ANIMATION)
             window['-DIE-2-'].update(source=dice.D6_ANIMATION)
             for _ in range(100):
